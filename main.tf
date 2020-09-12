3419605e3542a34a44ad5aca778744e04f7960a7
Anurag Gupta <anurag4516@gmail.com>
2020-09-12 18:15:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = var.region_of_deployment
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+ # access_key = var.aws_access_key
+  #secret_key = var.aws_secret_key
 }
 
